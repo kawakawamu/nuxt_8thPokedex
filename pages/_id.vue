e721d7c96fbc63bbbb9596f66d955a8a64a3c1b0
kata-n <kata.kawa813@gmail.com>
2020-08-03 07:44:57 +0900

    first commit

---
+++ pages/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <p>{{ message }}</p>
+    <p>{{ this.$route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function()
+  {
+    return {
+      message: "users/_id.vueを表示中"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <p>{{ message }}</p>
+    <hr>
+    <router-link to="/price">price page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      message: "Hello World"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
