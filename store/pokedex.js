eb6189ef3160b95d9582722e168c674b62d4fcb7
kata-n <kata.kawa813@gmail.com>
2020-08-06 07:10:54 +0900

    firestore接続, Actionの 作成

--- store/pokedex.js
+++ store/pokedex.js
@@ -1,5 +1,33 @@
 import firebase from '~/plugins/firebase'
 import { firestoreAction } from 'vuexfire'
+import { firestore } from 'firebase'
 
 const db = firebase.sirestore()
-const pokedex = db.collection('pokedex')
+const pokesRef = db.collection('pokedex')
+
+export const state = () => ({
+  pokedex: []
+})
+
+export const actions = {
+  init: firestoreAction(({ bindFirestoreRef }) => {
+    bindFirestoreRef('pokedex', pokesRef)
+  }),
+  add: firestoreAction((context, name) => {
+    if (name.trim()) {
+      todosRef.add({
+        name: name,
+        done: false,
+        created: firebase.firestore.FieldValue.serverTimestamp()
+      })
+    }
+  }),
+  remove: firestoreAction((context, id) => {
+    pokesRef.doc(id).delete()
+  }),
+  toggle: firestoreAction((context, poke) => {
+    pokesRef.doc(poke.id).update({
+      done: !poke.done
+    })
+  })
+}
